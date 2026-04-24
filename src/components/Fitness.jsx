05e8cb94e906c71c22df08ac1a1ce5606c20f404
Mzlh2 <shrutisoumya456@gmail.com>
2026-04-24 14:45:44 +0530

    Rendered info

--- src/components/Fitness.jsx
+++ src/components/Fitness.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import Data from '../Data'
 
 const Fitness = () => {
+  const FitnessData = Data.filter((item) => item.category === 'Fitness')
+  console.log(FitnessData)
   return (
-    <div>Fitness</div>
+    <div>{FitnessData.map((fitnessdata) => (
+      <>
+        <h1>{fitnessdata.title}</h1>
+        <img src={fitnessdata.img_url}/>
+      </>
+    ))} 
+    </div>
   )
 }
 
--- src/components/Food.jsx
+++ src/components/Food.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import Data from '../Data'
 
 const Food = () => {
+  const FoodData = Data.filter((item) => item.category === 'Food')
+  console.log(FoodData)
+
   return (
-    <div>Food</div>
+    <div>{FoodData.map((fooddata) => (
+      <>
+        <h1>{fooddata.title}</h1>
+        <img src={fooddata.img_url}/>
+      </>
+    ))}
+    </div>
   )
 }
 
